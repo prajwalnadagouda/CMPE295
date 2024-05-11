434fb08a2c0cb2829b73144f5cf0cdfe0fb714f3
Prajwal Nadagouda <32608138+prajwalnadagouda@users.noreply.github.com>
2024-05-10 22:48:11 -0700

    pushing from ML repo

---
+++ MachineLearning/RoutePlanning/app.py
@@ -0,0 +1,119 @@
+from flask import Flask, jsonify, request
+from datetime import datetime, timedelta
+from flask_cors import CORS
+import os.path
+import pickle
+import pandas as pd
+
+from routinghelp import getstartpincode
+from routinghelp import getaddress
+from routinghelp import getpath
+from prediction import getpredictions
+from routinghelp import getpeoplecount
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/update', methods=['POST'])
+def updateweights():
+    try:
+        data = request.json
+        newWeight=data
+        with open('./storage/weight.pkl', 'rb') as f:
+            listData = pickle.load(f)
+            oldWeight=listData[0]
+            oldCount=listData[1]
+        
+        oldWeight = {key: (value *oldCount) for key, value in oldWeight.items()}
+        
+        for key, new_value in newWeight.items():
+            key=int(key)
+            if key in oldWeight:
+                oldWeight[key] = oldWeight[key] + new_value
+            else:
+                print("issues")
+        oldWeight = {key: (value / (oldCount+1)) for key, value in oldWeight.items()}
+        # print(sum(oldWeight.values()))
+        with open('./storage/weight.pkl', 'wb') as f:
+            pickle.dump([oldWeight,oldCount+1], f)
+        return "SUCCESS"
+    except:
+        return "FAILURE"
+
+
+@app.route('/globalmodelstatus', methods=['GET'])
+def getglobalmodelstatus():
+    if(os.path.isfile("./nextpredict.csv")):
+        print("hello")
+        timeModified = os.path.getmtime("./nextpredict.csv")
+        modificationDatetime = datetime.fromtimestamp(timeModified)
+        formattedDatetime = modificationDatetime.strftime('%Y-%m-%d %H:%M:%S')
+        return formattedDatetime
+    else:
+        return "2024-04-29 01:30:23"
+    
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    try:
+        getpredictions()
+        return "SUCCESS"
+    except:
+        return "FAILURE"
+
+
+
+@app.route('/routes', methods=['POST'])
+def getroutes():
+    try:
+        Days=['Monday','Tuesday','Wednesday','Thursday','Friday']
+        routes={}
+        data = request.json
+        area=str(data['area']).replace(" ","")
+        print(area)
+        capacity=(data['capacity'])
+        areapincodes=getstartpincode(area)
+
+        currentDate = datetime.now()
+
+        for day in range(7):
+            if(currentDate.weekday()!=5 and currentDate.weekday()!=6):
+                dayRoute=[]
+                peopleCount=getpeoplecount(currentDate.strftime('%m/%d/%y'))
+                placecount=4
+                for i in range(placecount):
+                    timeDetails={}
+                    startPoint=areapincodes[i]
+                    endPoint=areapincodes[3-i]
+
+                    timeDetails['StartPoint']=getaddress(str(startPoint))
+                    timeDetails['EndPoint']=getaddress(str(endPoint))
+                    timeDetails['PeopleCount']=int(((placecount-i)*peopleCount)/10000)
+
+                    timeDetails['Stops']=[timeDetails['StartPoint']]
+                    path=getpath(timeDetails['StartPoint'],timeDetails['EndPoint'])
+                    for eachpath in path:
+                        timeDetails['Stops'].append(eachpath)
+                    timeDetails['Stops'].append(timeDetails['EndPoint'])
+                    dayRoute.append(timeDetails)
+                routes[currentDate.strftime("%A")]=dayRoute
+            currentDate=currentDate+timedelta(days=1)
+        return jsonify(routes)
+    except Exception as e:
+        print(e)
+        return jsonify({'error': 'Internal Server Error'}), 500
+    
+
+
+if __name__ == '__main__':
+    if(os.path.isfile("./storage/weight.pkl")):
+        pass
+    else:
+        df = pd.read_csv("bayareapincodes.csv")
+        zip_codes = df['ZipCodes'].unique()
+        equal_percentage = 100 / len(zip_codes)
+        zip_percentages = {zip_code: equal_percentage for zip_code in zip_codes}
+        with open('./storage/weight.pkl', 'wb') as f:
+            pickle.dump([zip_percentages,1], f)
+        print(zip_percentages)
+    app.run(host='0.0.0.0',port=8000, debug=True)
---
+++ MachineLearning/RoutePlanning/routinghelp.py
@@ -0,0 +1,73 @@
+import pickle
+import zipcodes
+import math 
+import pandas as pd
+
+#given area , get list of pincodes that belong that area.
+def getpincode(area):
+    with open('./storage/pincode_dictionary.pkl', 'rb') as f:
+        pincode_dict = pickle.load(f)
+    return(pincode_dict[area])
+
+
+#give address based on zipcode
+def getaddress(zipcode):
+    try:
+        zipcode=str(zipcode)
+        zipInfo=zipcodes.matching(str(zipcode))[0]
+        lat=zipInfo['lat']
+        long=zipInfo['long']
+        
+        try:
+            with open('./storage/zipcodeAddress.pkl', 'rb') as f:
+                zipcodeAddress = pickle.load(f)
+            address=(zipcodeAddress[zipcode])
+        except Exception as e:
+            address=str(zipcode)
+        return(str(address),str(lat),str(long))
+
+    except Exception as e:
+        print(e)
+        return str(zipcode)
+
+
+def betweenpoints(lat1, lon1, lat2, lon2):
+    lat1, lon1, lat2, lon2 = map(math.radians, [float(lat1), float(lon1), float(lat2), float(lon2)])
+    mid_lat = (lat1 + lat2) / 2
+    mid_lon = (lon1 + lon2) / 2
+    return str(math.degrees(mid_lat)), str(math.degrees(mid_lon))
+
+def  getpath(start,end):
+    try:
+        mLat,mLong=betweenpoints(start[1],start[2],end[1],end[2])
+        q1Lat,q1Long=betweenpoints(start[1],start[2],mLat,mLong)
+        q2Lat,q2Long=betweenpoints(mLat,mLong,end[1],end[2])
+        return[("near "+start[0],q1Lat,q1Long),("midway "+end[0],mLat,mLong),("near "+end[0],q2Lat,q2Long)]
+    except:
+        return []
+
+def getstartpincode(area):
+    try:
+        pincodeList=getpincode(area)
+        with open('./storage/weight.pkl', 'rb') as f:
+            weights = pickle.load(f)
+        weights=weights[0]
+        newdict={}
+        for pincode in pincodeList:
+            newdict[int(pincode)]=weights[int(pincode)]
+        newdict=dict(sorted(newdict.items(), key=lambda item: item[1]))
+        newlist=list(newdict.keys())
+        print([newlist[-1],newlist[-2],newlist[-3],newlist[-4]])
+        return [newlist[-1],newlist[-2],newlist[-3],newlist[-4]]
+    except Exception as e:
+        return []
+    
+
+def getpeoplecount(workingdate):
+    try:
+        df = pd.read_csv("./nextpredict.csv", parse_dates=['Date'], dayfirst=False)
+        idx=df.index[df['Date'] == str(workingdate)].tolist()[0]
+        countp = df['BusCommuters'].iloc[idx]
+        return int(countp)
+    except:
+        return 100
